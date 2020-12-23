dff8754854702eb03c150f63fb6179722c926a58
hato.popo <n4n.yk5.b@gmail.com>
2020-12-23 09:54:37 +0000

    ;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:edit, :update, :destroy]
+  before_action :require_user_logged_in, only: [:show, :edit, :update, :destroy, :followings, :followers, :likes]
   before_action :current_user, only:[:edit, :update, :destroy]
   
   def index
@@ -57,6 +57,8 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
+  
+  
   
   def likes
     @user = User.find(params[:id])
